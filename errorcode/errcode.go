7cc814d9b8d20b29f9a6cd2ef96473be81256036
冉文杰 <510495266@qq.com>
2020-07-05 20:05:48 +0800

    errorcode 改造

--- errorcode/errcode.go
+++ errorcode/errcode.go
@@ -1,10 +1,35 @@
 package errorcode
 
-type ErrorCode uint32
+import (
+	"encoding/json"
+)
 
-const (
-	OK 						ErrorCode	= 0
-	ErrorCodeInvalid 		ErrorCode	= 10001
-	ErrorCodeEngineExist 	ErrorCode	= 10002
-	ErrorCodeEngineNotExist 	ErrorCode	= 10003
+type ErrorCode struct {
+	Code 	uint32	`json:"code"`
+	Message string	`json:"message"`
+}
+
+func New(code uint32, message string) ErrorCode{
+	return ErrorCode{
+		Code:    code,
+		Message: message,
+	}
+}
+
+func (e ErrorCode)ToJson() []byte {
+	b,err := json.Marshal(&e)
+	if err != nil {
+		return []byte("")
+	}
+
+	return b
+}
+
+var (
+	OK 								ErrorCode	= New(0, "")
+	ErrorCodeInvalid 				ErrorCode	= New(10001, "")
+	ErrorCodeEngineExist 			ErrorCode	= New(10002, "")
+	ErrorCodeEngineNotExist 		ErrorCode	= New(10003, "")
+
+	ErrorCodeParamInvalidSymbol		ErrorCode = New(20001, "invalid symbol")
 )
